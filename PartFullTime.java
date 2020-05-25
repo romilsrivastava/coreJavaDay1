4a3a93cf067309ece24f35272c60264ee3c4d37f
romilsrivastava <64972037+romilsrivastava@users.noreply.github.com>
2020-05-25 20:00:06 +0530

    Update PartFullTime.java

--- PartFullTime.java
+++ PartFullTime.java
@@ -1,16 +1,16 @@
 public class PartFullTime {
 	public static void main(String[] args) {
 		int WagePerHour=20;
-		int FullDayHour=16;
-		int PartTimeHour=8;
+		int FullDayHour=8;
+		int PartTimeHour=4;
 		double randomc=Math.floor(Math.random() * 10) % 3;
 		int random= (int)randomc;
 		switch (random) {
 			case 1:
-			  System.out.println ("employee is present for full time");
-	        int DailyWageFullTime=( WagePerHour*FullDayHour );
-           System.out.println(DailyWageFullTime);
-			  break;
+			        System.out.println ("employee is present for full time");
+	                        int DailyWageFullTime=( WagePerHour*FullDayHour );
+                                System.out.println(DailyWageFullTime);
+			        break;
 			case 2:
 				System.out.println("employee is present for part time");
 				int DailyWagePartTime=( WagePerHour*PartTimeHour );
